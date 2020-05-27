76ccaaef35111f37aa33703d840162c1dd9c3f20
Bruno Teixeira <bruno@MacBook-Pro-de-Bruno.local>
2020-05-27 16:26:45 -0300

    Melhorias gerais

--- templateApp/views.py
+++ templateApp/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from .forms import templateForm
 from .models import templateModel
-# Create your views here.
+
 
 def templateView(request):
 	name='Você pode me alterar na view.'
 	return render(request, "templateApp/templateView.html", {'nome':name})
-
