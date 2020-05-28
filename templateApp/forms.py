374e82a00a02fda9d5363c5116494df2d00cc464
Bruno Teixeira <bruno@MacBook-Pro-de-Bruno.local>
2020-05-27 23:44:28 -0300

    Adicionadas informações de sites e referências

--- templateApp/forms.py
+++ templateApp/forms.py
@@ -2,3 +2,11 @@ from django import forms
 
 class templateForm(forms.Form):
 	name = forms.CharField(max_length=100)
+
+# Form Field Reference: https://docs.djangoproject.com/en/3.0/ref/forms/fields/
+
+# forms.CharField(max_length=XXX, required=False|True, label='NNNN')
+# forms.ModelChoiceField(queryset=XXXX.order_by('XXXX'), empty_label='NNNNN', required=False|True,label='NNNN')
+# forms.ImageField()
+# forms.BooleanField()
+
--- templateApp/models.py
+++ templateApp/models.py
@@ -5,3 +5,11 @@ class templateModel(models.Model):
 	name = models.CharField(max_length=100)
 	def __str__(self):
 		return self.name
+
+# Model Field Reference: https://docs.djangoproject.com/en/3.0/ref/models/fields/
+
+# Linha de texto:	models.CharField(max_length=XXX)
+# Sim / Não:		models.BooleanField()
+# Campos de Texto:	models.TextField()
+# Imagens:			models.ImageField(upload_to='/static/uploads')
+# Relacionamento:	models.ForeignKey(NOME_MODELO, on_delete=PROTECT)
